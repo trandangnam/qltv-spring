045b75159d561779d568a5ab0ddd84b4615b56d1
Nhien <nhienau@gmail.com>
2024-05-05 22:32:36 +0700

    fix

--- src/main/resources/static/assets/js/pages/index.js
+++ src/main/resources/static/assets/js/pages/index.js
@@ -12,3 +12,7 @@ const section1 = document.getElementById("section--1");
 btnScrollTo.addEventListener("click", function () {
   section1.scrollIntoView({ behavior: "smooth" });
 });
+
+if (window.location.search !== "") {
+  section1.scrollIntoView({ behavior: "instant" });
+}
